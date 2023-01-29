9fe6815c27cd65afae023b85354cc6bb106de5cd
DmitryGil <91392061+dmitrygil1@users.noreply.github.com>
2023-01-29 12:56:21 +0300

    fix: try to find bug

--- src/index.js
+++ src/index.js
@@ -39,8 +39,8 @@ const MORSE_TABLE = {
 function decoder(expr) {
   let decodeWordsArrg = [];
   let finaleDecoderArrg = [];
-  for (let i = 1; i <= expr.split("").length/10;i++) {
-    let wordArray = expr.split("").slice(`${i-1}0`,`${i}0`);
+  for (let i = 1; i <= expr.split("").length / 10; i++) {
+    let wordArray = expr.split("").slice(`${ i-1 }0`,`${i}0`);
     let morseWord = [];
     for (let m = 0; m < wordArray.length; m += 2) {
       if (wordArray[m] + wordArray[m + 1] === "10") {
