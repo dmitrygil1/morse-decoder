78fcfe15722516eec2508b792130f81fdaee6a3c
DmitryGil <91392061+dmitrygil1@users.noreply.github.com>
2023-01-29 12:59:19 +0300

    fix: change script logic

--- src/index.js
+++ src/index.js
@@ -36,30 +36,21 @@ const MORSE_TABLE = {
   "----.": "9",
   "-----": "0",
 };
-function decoder(expr) {
-  let decodeWordsArrg = [];
-  let finaleDecoderArrg = [];
-  for (let i = 1; i <= expr.split("").length / 10; i++) {
-    let wordArray = expr.split("").slice(`${ i-1 }0`,`${i}0`);
-    let morseWord = [];
-    for (let m = 0; m < wordArray.length; m += 2) {
-      if (wordArray[m] + wordArray[m + 1] === "10") {
-        morseWord.push(".");
-      } else if (wordArray[m] + wordArray[m + 1] === "11") {
-        morseWord.push("-");
-      }}
-  decodeWordsArrg.push(morseWord);
-  }
-  decodeWordsArrg.forEach((elements) => {
-    let valueElement = elements.join("");
-    if (MORSE_TABLE.hasOwnProperty(valueElement) == false) {
-      finaleDecoderArrg.push(" ");
-    } else {
-      finaleDecoderArrg.push(MORSE_TABLE[valueElement]);
-    }
-  });
-  return finaleDecoderArrg.join("");
+function decode(expr) {
+    let strMorseWord = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        let morseLetter = expr.slice(i, i+10);
+        morseLetter = morseLetter.replace(/10/g, '.');
+        morseLetter = morseLetter.replace(/11/g, '-');
+        morseLetter = morseLetter.replace(/0/g, '');
+        if (morseLetter === '**********') {
+            strMorseWord += " ";
+        } else {
+            strMorseWord += MORSE_TABLE[morseLetter];
+        }
+      }
+    return strMorseWord;
 }
 module.exports = {
-  decoder,
-};
+    decode
+}
